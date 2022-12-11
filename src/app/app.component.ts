720b034e24fb0a845cf41730516f536e422cdef5
Татьяна <tatyana@iMac-Tatana.local>
2022-12-11 12:32:37 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,90 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cars-app';
+  priceForm = this.fb.group({
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+    car: ['', Validators.required],
+  })
+
+carsData = [
+ {
+  image: "1.png",
+  name: "Lamborghini Huracan Spyder",
+  gear: "полный",
+  engine: 5.2,
+  places: 2
+ },
+ {
+  image: "2.png",
+  name: "Chevrolet Corvette",
+  gear: "полный",
+  engine: 6.2,
+  places: 2
+ },
+ {
+  image: "3.png",
+  name: "Ferrari California",
+  gear: "полный",
+  engine: 3.9,
+  places: 4
+ },
+ {
+  image: "4.png",
+  name: "Lamborghini Urus",
+  gear: "полный",
+  engine: 4.0,
+  places: 5
+ },
+ {
+  image: "5.png",
+  name: "Audi R8",
+  gear: "полный",
+  engine: 5.2,
+  places: 2
+ },
+ {
+  image: "6.png",
+  name: "Chevrolet Camaro",
+  gear: "полный",
+  engine: 2.0,
+  places: 4
+ }
+ 
+];
+
+  constructor(private fb: FormBuilder){
+    
+  }
+
+
+
+  //goScroll(target: HTMLElement){
+   // target.scrollIntoView({behavior: "smooth"});
+  //}
+  goScroll(target: HTMLElement, car?: any) {
+    target.scrollIntoView({behavior: "smooth"});
+    if (car) {
+      this.priceForm.patchValue({car: car.name});
+    }
+  }
+  trans: any;
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.trans = {transform: 'translate3d(' + ((e.clientX * 0.3) / 8) + 'px,' + ((e.clientY * 0.3) / 8) + 'px,0px)'};
+  }
+  
+  bgPos: any;
+  @HostListener('document:scroll', ['$event'])
+  onScroll() {
+    this.bgPos = {backgroundPositionX: '0' + (0.3 * window.scrollY) + 'px'};
+  }
+
+  onSubmit() {
+    if (this.priceForm.valid){
+      alert("Спасибо за заявку. Мы свяжемся с вами в ближайшее время");
+      this.priceForm.reset();
+    }
+  }
+  //title = 'cars-app';
 }
